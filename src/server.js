33df56794bddf4f335e93f63015c6b686162ff06
Tania Dumych <tanja11.malvchko@gmail.com>
2025-07-20 10:51:00 +0200

    commit ten

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 
-
 import contactsRouter from './routers/contacts.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
@@ -11,20 +10,14 @@ function setupServer() {
   const app = express();
 
   app.use(cors());
-  app.use(pino);
+  app.use(pino());
   app.use(express.json());
 
   app.use('/contacts', contactsRouter);
-
   app.use(notFoundHandler);
-
   app.use(errorHandler);
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+  return app;
 }
 
 export default setupServer;
