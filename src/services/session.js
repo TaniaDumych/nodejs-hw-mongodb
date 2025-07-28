72e744c391d91070cc3a8b73c2a493a69f8d0ff3
Tania Dumych <tanja11.malvchko@gmail.com>
2025-07-28 23:17:22 +0200

    commit 17

--- src/services/session.js
+++ src/services/session.js
@@ -1,29 +1,43 @@
 import Session from '../models/Session.js';
 import jwt from 'jsonwebtoken';
 
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET; 
-if (!ACCESS_TOKEN_SECRET) {
-  throw new Error('ACCESS_TOKEN_SECRET is not set in environment variables');
+const ACCESS_TOKEN_EXPIRE = 15 * 60;
+const REFRESH_TOKEN_EXPIRE = 30 * 24 * 60 * 60;
+
+function getAccessTokenSecret() {
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  if (!secret) {
+    throw new Error('ACCESS_TOKEN_SECRET is not set in environment variables');
+  }
+  return secret;
 }
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'refresh-secret';
 
-
-const ACCESS_TOKEN_EXPIRE = 15 * 60;          
-const REFRESH_TOKEN_EXPIRE = 30 * 24 * 60 * 60; 
-
+function getRefreshTokenSecret() {
+  return process.env.REFRESH_TOKEN_SECRET || 'refresh-secret';
+}
 
 export function generateTokens(userId) {
   const accessTokenExp = new Date(Date.now() + ACCESS_TOKEN_EXPIRE * 1000);
   const refreshTokenExp = new Date(Date.now() + REFRESH_TOKEN_EXPIRE * 1000);
 
-  const accessToken = jwt.sign({ userId }, ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRE });
-  const refreshToken = jwt.sign({ userId }, REFRESH_TOKEN_SECRET, { expiresIn: REFRESH_TOKEN_EXPIRE });
+  const accessToken = jwt.sign({ userId }, getAccessTokenSecret(), {
+    expiresIn: ACCESS_TOKEN_EXPIRE,
+  });
+
+  const refreshToken = jwt.sign({ userId }, getRefreshTokenSecret(), {
+    expiresIn: REFRESH_TOKEN_EXPIRE,
+  });
 
   return { accessToken, refreshToken, accessTokenExp, refreshTokenExp };
 }
 
-
-export async function saveSession({ userId, accessToken, refreshToken, accessTokenValidUntil, refreshTokenValidUntil }) {
+export async function saveSession({
+  userId,
+  accessToken,
+  refreshToken,
+  accessTokenValidUntil,
+  refreshTokenValidUntil,
+}) {
   const session = new Session({
     userId,
     accessToken,
@@ -35,16 +49,13 @@ export async function saveSession({ userId, accessToken, refreshToken, accessTok
   return session.save();
 }
 
-
 export async function deleteSessionByUserId(userId) {
   return Session.deleteMany({ userId });
 }
 
-
 export async function findSessionByRefreshToken(refreshToken) {
   return Session.findOne({ refreshToken });
 }
-
 
 export async function deleteSessionById(sessionId) {
   return Session.findByIdAndDelete(sessionId);
