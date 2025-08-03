be6bd243047e37dc039c603b360da7cb412780da
Tania Dumych <tanja11.malvchko@gmail.com>
2025-08-03 13:50:21 +0200

    part

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -4,7 +4,8 @@ const { SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, SMTP_FROM } = process.en
 
 const transport = nodemailer.createTransport({
   host: SMTP_HOST,
-  port: SMTP_PORT,
+  port: Number(SMTP_PORT),
+  secure: true,
   auth: {
     user: SMTP_USER,
     pass: SMTP_PASSWORD,
